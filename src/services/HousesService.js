4921b3817096e1f3bec039e6b4efdf6ef05854bc
James54509 <jamesc@newvisualmedia.com>
2024-09-10 16:16:59 -0600

    pagination for houses seems to be working but is probably not structured very well

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -18,8 +18,30 @@ class HousesService {
         const sortBy = query.sortBy
         delete query.sortBy
         
-        const houses = await dbContext.Houses.find(query).sort(sortBy + 'createdAt') 
-        return houses
+        // const houses = await dbContext.Houses.find(query).sort(sortBy + 'createdAt') 
+        // console.log(query)
+        // return houses
+        const pageNumber = parseInt(query.page) || 1
+        const limitAmount = 10
+        const skipAmount = (pageNumber -1) * limitAmount
+        delete query.page
+
+        const houses = await dbContext.Houses.find(query)
+            .sort(sortBy + 'createdAt')
+            .skip(skipAmount)
+            .limit(limitAmount)
+
+        const houseCount = await dbContext.Houses.countDocuments()
+
+        return{
+            results: houses,
+            count: houseCount,
+            currentPage: pageNumber, 
+            totalPages: Math.ceil(houseCount / limitAmount)
+        }
+
+    
+    
     }
 }
 
